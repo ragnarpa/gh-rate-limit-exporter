04f765bde3eba8254d26bbe71b8a33160a521542
Ragnar Paide <16119863+ragnarpa@users.noreply.github.com>
2023-01-07 22:58:56 +0200

    Implement pure Prometheus collector (#12)

--- pkg/exporter/handler_test.go
+++ pkg/exporter/handler_test.go
@@ -5,56 +5,21 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/common/expfmt"
-	"github.com/ragnarpa/gh-rate-limit-exporter/pkg/github"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewMetricsHandler(t *testing.T) {
-	t.Run("registers collectors", func(t *testing.T) {
-		r := prometheus.NewRegistry()
-		c := NewCollector(CollectorParams{})
-
-		NewMetricsHandler(c, r)
-
-		for _, c := range c.Collectors() {
-			assert.True(t, r.Unregister(c))
-		}
-
-		assert.Greater(t, len(c.Collectors()), 0, "no collectors")
-	})
-}
-
 func TestMetricsHandlerServeHTTP(t *testing.T) {
-	t.Run("implements http.Handler and serves metrics", func(t *testing.T) {
+	t.Run("implements http.Handler and returns 200", func(t *testing.T) {
 		c := NewCollector(CollectorParams{})
-		var h http.Handler = NewMetricsHandler(c, prometheus.NewRegistry())
-		rl := &github.RateLimit{
-			Resource:          "resource",
-			Limit:             1000,
-			Remaining:         1000,
-			Reset:             time.Now(),
-			AppName:           "name",
-			AppKind:           "kind",
-			AppID:             "id",
-			AppInstallationID: "installationid",
-		}
-
-		c.SetRateLimit(rl)
-		c.SetRateLimitRemaining(rl)
-		c.SetRateLimitUsage(rl)
+		reg := prometheus.NewRegistry()
+		var h http.Handler = NewMetricsHandler(c, reg)
 
 		rr := httptest.NewRecorder()
 		req := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(""))
 		h.ServeHTTP(rr, req)
 
-		tp := &expfmt.TextParser{}
-		mfs, err := tp.TextToMetricFamilies(rr.Result().Body)
-
-		assert.NoError(t, err)
-		assert.Len(t, mfs, 3)
+		assert.Equal(t, 200, rr.Result().StatusCode)
 	})
 }
